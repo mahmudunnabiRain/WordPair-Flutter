f3a3fd4705b81d29a512452e21dd124faa54426d
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-05-05 02:13:30 +0600

    update 01

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,16 +23,6 @@ class _MyHomePageState extends State<MyHomePage> {
   final _randomWordPairs = <WordPair>[];
   final _savedWordPairs = Set<WordPair>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-    });
-  }
-
   void _pushSaved() {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-        primaryColor: Colors.purple[900],
+        primaryColor: Colors.green[900],
       ),
       home: MyHomePage(title: 'WordPair Generator'),
     );
