3bb3c99c2f896b0eae4c49cf5e6fa2b002134d2a
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-05-05 02:09:03 +0600

    completed v.10

---
+++ lib/homepage.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+    });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
+
+      final List<Widget> devided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved WordPairs'),
+          ),
+          body: ListView(children: devided));
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildList(),
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          final index = item ~/ 2;
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool _alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: TextStyle(fontSize: 18.0),
+        ),
+        trailing: Icon(_alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: _alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (_alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
+  }
+}
